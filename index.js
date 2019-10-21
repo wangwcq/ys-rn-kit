8eeb565c970b62ad43ee6e0512e01f6bce038e1b
Chris Wang <chris.wang@mioying.com>
2019-10-21 12:37:08 +0800

    ER: Add register

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import Loading from './screens/Loading';
@@ -10,28 +10,48 @@ import _ from 'lodash';
 
 import initStore from './store/index';
 
-const { store, persistor } = initStore({
-});
+let store = null;
+let persistor = null;
 
 // dev only tools
 const purge = () => persistor.purge();
 
-const App: () => {
-  return (
-    <Provider store={store}>
-    <PersistGate loading={Loading} persistor={persistor}>
-    <ThemedApp />
-    </PersistGate>
-    </Provider>
-  );
+/**
+ * register
+ * @param options
+ * @param {boolean} options.purge
+ * @returns {function(): *}
+ */
+const register = (options = {}) => {
+  if (options.purge) purge();
+
+  const {
+    store: vStore,
+    persistor: vPersistor,
+  } = initStore();
+  store = vStore;
+  persistor = vPersistor;
+
+  AppRegistry.registerComponent(consts.appKey, () => () => {
+    return (
+      <Provider store={store}>
+        <PersistGate
+          loading={Loading}
+          persistor={persistor}
+        >
+          <ThemedApp/>
+        </PersistGate>
+      </Provider>
+    );
+  });
 };
 
 export {
-  App,
   store,
   persistor,
   purge,
   _,
   consts,
   initConsts,
+  register,
 };
